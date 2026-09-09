5819222c9917005d9fe97075c76aa35e134fac44
Liana <liana80.dev@gmail.com>
2026-09-09 18:53:51 +0330

    feat:serve fontend  static files

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,6 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
 import cors from "cors";
 import express from "express";
 import notFoundMiddleware from "./middleware/notFound.middleware.js";
@@ -7,6 +10,11 @@ import authRoutes from "./routes/auth.routes.js";
 import contactRoutes from "./routes/contact.routes.js";
 import projectRoutes from "./routes/project.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootDir = path.resolve(__dirname, "../../");
+
 
  
 
@@ -25,6 +33,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/messages", messageRoutes);
+
+app.use(express.static(path.join(rootDir, "dist")));
+
+app.get("*", (req, res, next) => {
+  if (req.path.startsWith("/api")) return next();
+  res.sendFile(path.join(rootDir, "dist", "index.html"));
+});
+
 
 
 app.use(notFoundMiddleware);
